dc88d0384edfff7f0597dcc6c3828568cca7bbf8
João Pedro Navarro Okita <joao.okita07@unifesp.br>
2026-08-27 17:53:52 -0300

    Add files via upload

---
+++ flor.js
@@ -0,0 +1,241 @@
+const canvas = document.getElementById("canvas");
+const gl = canvas.getContext("webgl2");
+
+if (!gl) {
+    throw new Error("WebGL 2 não é suportado.");
+}
+
+
+// --------------------------------------------------
+// 1. VERTICES
+// --------------------------------------------------
+
+// Definindo os vértices do círculo
+
+let coord_xy_pontos_circulo = [];
+
+let cores_pontos_circulo = [];
+
+const num_pontos_circulo = 12;
+
+const centro_circulo = [0, 0];
+
+const raio = 0.5;
+
+const dx = (2*raio)/num_pontos_circulo;
+
+let x = centro_circulo - raio;
+let y = 0.0;
+for(let i = 0; i < num_pontos_circulo; i++) {
+    y = Math.sqrt(Math.pow(raio, 2) - Math.pow((x - centro_circulo[0]), 2));
+    coord_xy_pontos_circulo.push(x, y + centro_circulo[1]);
+    coord_xy_pontos_circulo.push(x, -y + centro_circulo[1]);
+
+    cores_pontos_circulo.push(1.0)
+    cores_pontos_circulo.push(0.0)
+    cores_pontos_circulo.push(0.0)
+
+    x = x + dx;
+}
+
+
+const vertices = new Float32Array(coord_xy_pontos_circulo);
+
+
+// --------------------------------------------------
+// 1. CORES
+// --------------------------------------------------
+
+const colors = new Float32Array(cores_pontos_circulo);
+
+
+// --------------------------------------------------
+// 2. BUFFERS
+// --------------------------------------------------
+
+const verticesBuffer = gl.createBuffer();
+
+gl.bindBuffer(gl.ARRAY_BUFFER, verticesBuffer);
+
+gl.bufferData(
+    gl.ARRAY_BUFFER,
+    vertices,
+    gl.STATIC_DRAW
+);
+
+const colorsBuffer = gl.createBuffer();
+
+gl.bindBuffer(gl.ARRAY_BUFFER, colorsBuffer);
+
+gl.bufferData(
+    gl.ARRAY_BUFFER,
+    colors,
+    gl.STATIC_DRAW
+);
+
+
+// --------------------------------------------------
+// 3. VERTEX SHADER
+// --------------------------------------------------
+
+const vertexShaderSource = `#version 300 es
+
+in vec2 aPosition;
+in vec3 aColor;
+
+out vec3 vColor;
+
+void main() {
+    gl_Position = vec4(aPosition, 0.0, 1.0);
+    vColor = aColor;
+}
+
+`;
+
+
+// --------------------------------------------------
+// 4. FRAGMENT SHADER
+// --------------------------------------------------
+
+const fragmentShaderSource = `#version 300 es
+
+precision mediump float;
+
+in vec3 vColor;
+
+out vec4 outColor;
+
+void main() {
+    outColor = vec4(vColor, 1.0);
+}
+
+`;
+
+
+// --------------------------------------------------
+// 5. COMPILAR SHADERS
+// --------------------------------------------------
+
+function createShader(gl, type, source) {
+
+    const shader = gl.createShader(type);
+
+    gl.shaderSource(shader, source);
+
+    gl.compileShader(shader);
+
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+
+        const error = gl.getShaderInfoLog(shader);
+
+        gl.deleteShader(shader);
+
+        throw new Error(error);
+    }
+
+    return shader;
+}
+
+
+const vertexShader = createShader(
+    gl,
+    gl.VERTEX_SHADER,
+    vertexShaderSource
+);
+
+const fragmentShader = createShader(
+    gl,
+    gl.FRAGMENT_SHADER,
+    fragmentShaderSource
+);
+
+
+// --------------------------------------------------
+// 6. CRIAR PROGRAMA
+// --------------------------------------------------
+
+const program = gl.createProgram();
+
+gl.attachShader(program, vertexShader);
+gl.attachShader(program, fragmentShader);
+
+gl.linkProgram(program);
+
+if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+
+    throw new Error(
+        gl.getProgramInfoLog(program)
+    );
+}
+
+
+// --------------------------------------------------
+// 7. LOCAL DOS ATRIBUTOS
+// --------------------------------------------------
+
+const positionLocation =
+    gl.getAttribLocation(
+        program,
+        "aPosition"
+    );
+
+const colorLocation =
+    gl.getAttribLocation(
+        program,
+        "aColor"
+    );
+
+
+// --------------------------------------------------
+// 8. CONFIGURAR ATRIBUTOS
+// --------------------------------------------------
+
+gl.bindBuffer(gl.ARRAY_BUFFER, verticesBuffer);
+
+gl.enableVertexAttribArray(positionLocation);
+
+gl.vertexAttribPointer(
+    positionLocation,
+    2,
+    gl.FLOAT,
+    false,
+    0,
+    0
+);
+
+gl.bindBuffer(gl.ARRAY_BUFFER, colorsBuffer);
+
+gl.enableVertexAttribArray(colorLocation);
+
+gl.vertexAttribPointer(
+    colorLocation,
+    3,
+    gl.FLOAT,
+    false,
+    0,
+    0
+);
+
+
+// --------------------------------------------------
+// 9. LIMPAR TELA
+// --------------------------------------------------
+
+gl.clearColor(0.1, 0.1, 0.1, 1.0);
+
+gl.clear(gl.COLOR_BUFFER_BIT);
+
+
+// --------------------------------------------------
+// 10. DESENHAR
+// --------------------------------------------------
+
+gl.useProgram(program);
+
+const numComponents = 2;
+
+gl.drawArrays(
+    gl.TRIANGLES,
+    0,
+    vertices.length / numComponents
+);
